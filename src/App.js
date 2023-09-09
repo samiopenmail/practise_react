b32361b6331816c972ee16dc17510da0cde16b14
DESKTOP-GQF5OGA\sami <samiopenmail@gmail.com>
2023-09-09 07:42:49 +0600

    test commit nothing special

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Card from "./components/Card";
 function App(){
   return (
-    <Card Title="Title goes here" Desc="This is sami description" />
+    <Card Title="Title goes here.." Desc="This is sami description" />
   );
 }
 export default App;
