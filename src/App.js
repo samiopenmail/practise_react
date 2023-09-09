3587105bacefff050926a2dd19766ce60099ec4e
DESKTOP-GQF5OGA\sami <samiopenmail@gmail.com>
2023-09-09 07:50:39 +0600

    Revert "1"
    This reverts commit 959cfc9d21e34564d6968de5ac069cf8a787f157.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import Card from "./components/Card";
 function App(){
   return (
-<<<<<<< HEAD
     <Card Title="Title goes here...Second test" Desc="This is sami description" />
-=======
-    <Card Title="Title goes here" Desc="This is sami description" />
->>>>>>> parent of b32361b (test commit nothing special)
   );
 }
 export default App;
